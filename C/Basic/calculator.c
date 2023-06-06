9409e13011a8e7bcb4d804f80bcdf3db9d0845bf
wrench <133785769+w3nch@users.noreply.github.com>
2023-06-06 11:30:46 +0530

    Update calculator.c

--- C/Basic/calculator.c
+++ C/Basic/calculator.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
-    char operator;
-    double num1, num2, result;
+  char operator;
+  double num1, num2, result;
 
-    printf("Enter an operator (+ - * /): ");
-    scanf(" %c", &operator);
+  printf("\nEnter an operator (+ - * /): ");
+  scanf(" %c", &operator);
 
-    printf("Enter number 1: ");
-    scanf("%lf", &num1);
+  printf("Enter number 1: ");
+  scanf("%lf", &num1);
 
-    printf("Enter number 2: ");
-    scanf("%lf", &num2);
+  printf("Enter number 2: ");
+  scanf("%lf", &num2);
 
-    result = (operator == '+') ? (num1 + num2)
-             : (operator == '-') ? (num1 - num2)
-             : (operator == '*') ? (num1 * num2)
-             : (operator == '/') ? (num1 / num2)
-                                 : printf("Invalid operator.\n");
+  result = (operator == '+') ? (num1 + num2) :
+           (operator == '-') ? (num1 - num2) :
+           (operator == '*') ? (num1 * num2) :
+           (operator == '/') ? (num1 / num2) :
+           0;
 
-    printf("The result is %.2lf.\n", result);
-
-    return 0;
+  printf("The result is: %.2lf\n", result);
+  return 0;
 }
